ba9eac421dbac2a9beebed3733f62a6280f76974
Jeremy Zhang <jeremyziyuzhang@gmail.com>
2017-01-06 19:25:49 -0500

    Initial Commit

---
+++ SmartCarOAuthSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartCarOAuthSDK
+//
+//  Created by Ziyu Zhang on 1/6/17.
+//  Copyright © 2017 Ziyu Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
