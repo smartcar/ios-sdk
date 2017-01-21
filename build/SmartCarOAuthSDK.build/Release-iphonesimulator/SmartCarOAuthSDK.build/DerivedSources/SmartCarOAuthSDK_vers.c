7be81906a38f2ba07c9cbe4cbc7b1f7ee7d09d81
Jeremy Zhang <jeremyziyuzhang@gmail.com>
2017-01-20 20:11:50 -0500

    travis-ci integration

---
+++ build/SmartCarOAuthSDK.build/Release-iphonesimulator/SmartCarOAuthSDK.build/DerivedSources/SmartCarOAuthSDK_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char SmartCarOAuthSDKVersionString[];
+ extern const double SmartCarOAuthSDKVersionNumber;
+
+ const unsigned char SmartCarOAuthSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SmartCarOAuthSDK  PROJECT:SmartCarOAuthSDK-1" "\n";
+ const double SmartCarOAuthSDKVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/SmartCarOAuthSDK.build/Release-iphonesimulator/SmartCarOAuthSDK.build/Objects-normal/x86_64/SmartCarOAuthSDK_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jeremyzhang/Documents/XCodeProjects/iOS10/SmartCarOAuthSDK/build/SmartCarOAuthSDK.build/Release-iphonesimulator/SmartCarOAuthSDK.build/DerivedSources/SmartCarOAuthSDK_vers.c
