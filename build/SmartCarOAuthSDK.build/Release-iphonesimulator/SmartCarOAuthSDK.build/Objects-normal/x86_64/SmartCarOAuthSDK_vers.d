2a7f49f66e439e4f4dd9b9c4756bb2098add07e9
Jeremy Zhang <jeremyziyuzhang@gmail.com>
2017-01-24 15:20:34 -0500

    added gitignore

--- build/SmartCarOAuthSDK.build/Release-iphonesimulator/SmartCarOAuthSDK.build/Objects-normal/x86_64/SmartCarOAuthSDK_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jeremyzhang/Documents/XCodeProjects/iOS10/SmartCarOAuthSDK/build/SmartCarOAuthSDK.build/Release-iphonesimulator/SmartCarOAuthSDK.build/DerivedSources/SmartCarOAuthSDK_vers.c
