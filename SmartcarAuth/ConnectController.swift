8a25157ad2796543136ba232cdad0ffcab38645a
ew534679 <166439613+ew534679@users.noreply.github.com>
2025-01-27 11:03:24 -0600

    display webview in its own UVC
    this fixes lifecycle/object retention bugs

--- SmartcarAuth/ConnectController.swift
+++ SmartcarAuth/ConnectController.swift
@@ -25,68 +25,78 @@ import WebKit
 import UIKit
 
 /**
- A controller responsible for:
+ A view controller responsible for:
  
  - Initializing and presenting the WKWebView.
  - Detecting redirects back to the application (via `webView(_:decidePolicyFor:decisionHandler:)`).
  - Invoking the callback closure when the application redirect is encountered.
  */
 @objcMembers
-public class ConnectController: NSObject, WKNavigationDelegate {
+public class ConnectController: UIViewController, WKNavigationDelegate {
     public var webView: WKWebView!
-    
-    private var viewController: UIViewController
+
     private var authUrl: URL
     private var redirectUriHost: String
     private var handleCallback: (URL) -> Void
-    
+
     // An instance of `OAuthCapture` which handles JS messaging and OEM login flow.
     private var oauthCapture: OAuthCapture?
 
+
     /**
      Initializes the ConnectController.
-     - parameters:
-       - viewController: the UIViewController responsible for presenting Smartcar Connect.
-       - authUrl: the Connect URL to open in the WKWebView.
-       - redirectUriHost: host of the redirect URI that was passed to SmartcarAuth.
-       - handleCallback: callback function that is invoked upon redirect back to the application.
+     
+     - Parameters:
+       - authUrl: The Connect URL to open in the WKWebView.
+       - redirectUriHost: Host of the redirect URI that was passed to SmartcarAuth.
+       - handleCallback: Callback function that is invoked upon redirect back to the application.
      */
-    public init(viewController: UIViewController,
-                authUrl: URL,
+    public init(authUrl: URL,
                 redirectUriHost: String,
                 handleCallback: @escaping (URL) -> Void) {
-        self.viewController = viewController
         self.authUrl = authUrl
         self.redirectUriHost = redirectUriHost
         self.handleCallback = handleCallback
-        
-        super.init()
-        
-        // Configure a WKWebView.
-        let webviewConfiguration = WKWebViewConfiguration()
-        self.webView = WKWebView(
-            frame: CGRect(x: 0, y: 50,
-                          width: Int(viewController.view.frame.width),
-                          height: Int(viewController.view.frame.height)),
-            configuration: webviewConfiguration
-        )
-        self.webView.scrollView.contentInsetAdjustmentBehavior = .never
-        self.webView.navigationDelegate = self
-        
-        // Instantiate OAuthCapture (which handles script messaging + OEM flows)
-        // and attach it to this webView's userContentController.
-        self.oauthCapture = OAuthCapture(webView: self.webView)
+        super.init(nibName: nil, bundle: nil)
     }
-    
+
+    // Required initializer for using storyboard/XIB (not used here)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupWebView()
+    }
+
     /**
      Loads the Connect URL into the web view and presents it.
      */
-    public func launchWebView() {
-        let request = URLRequest(url: self.authUrl)
-        self.webView.load(request)
-        self.viewController.view.addSubview(webView)
+    private func setupWebView() {
+        let webviewConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webviewConfiguration)
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+        
+        // Constrain the webView to the safe area
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        // Instantiate OAuthCapture (which handles script messaging + OEM flows)
+        oauthCapture = OAuthCapture(webView: webView)
+        
+        // Start web view
+        let request = URLRequest(url: authUrl)
+        webView.load(request)
     }
-    
+
     /**
      WKNavigationDelegate method: listens for navigation changes.
      If the navigation is to the redirect URI (matching `redirectUriHost`),
@@ -96,18 +106,19 @@ public class ConnectController: NSObject, WKNavigationDelegate {
                         decidePolicyFor navigationAction: WKNavigationAction,
                         decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         
-        guard let host = navigationAction.request.url?.host,
+        guard let url = navigationAction.request.url,
+              let host = url.host,
               host == self.redirectUriHost else {
-            // allow navigation in all other cases
+            // Allow navigation in all other cases
             decisionHandler(.allow)
             return
         }
         
-        // If the navigation is the app redirect, cancel and call back
+        // If the navigation is the app redirect, cancel and handle callback
         decisionHandler(.cancel)
-        self.handleCallback(navigationAction.request.url!)
+        self.handleCallback(url)
         
-        // Remove the webView from the view
-        self.webView.removeFromSuperview()
+        // Dismiss the ConnectController
+        self.dismiss(animated: true, completion: nil)
     }
 }
