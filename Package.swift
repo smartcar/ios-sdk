942dc21c0762ad65ed4b247dc2349ec7c59e57dd
buddy <buddy@buddys-Virtual-Machine.local>
2025-09-25 22:13:33 +0000

    Release version v6.2.1

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let version = "v6.2.1"
+let checksum = "9d39fca6cba2f3c9f534876f7a4e7c93e9080829f11848beefe295be55eff29c"
+
+let package = Package(
+    name: "SmartcarAuth",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "SmartcarAuth",
+            targets: ["SmartcarAuth"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Nimble.git", from: "13.7.1")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SmartcarAuthBinary",
+            url: "https://github.com/smartcar/ios-sdk/releases/download/\(version)/framework.zip",
+            checksum: checksum
+        ),
+        .target(
+            name: "SmartcarAuth",
+            dependencies: ["SmartcarAuthBinary"],
+            path: "SmartcarAuth"
+        ),
+        .testTarget(
+            name: "SmartcarAuthTests",
+            dependencies: [
+                "SmartcarAuth",
+                .product(name: "Nimble", package: "Nimble")
+            ],
+            path: "SmartcarAuthTests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
